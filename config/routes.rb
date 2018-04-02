a9002171a755b2057e0cca1e265bd10b40171f83
stevengangano <stevengangano@yahoo.com>
2018-04-02 13:12:33 -0700

    added devise and initial layout for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  
+  devise_for :users, path: '', path_names: { sign_in: 'login',
+  sign_out: 'logout', sign_up: 'register'}
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
