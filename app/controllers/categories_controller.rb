296ea3bcd654aa6908d950a933a9ca4838f84346
stevengangano <stevengangano@yahoo.com>
2018-04-06 14:38:33 -0700

    added categories to index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,13 @@
 class CategoriesController < ApplicationController
+  # before_action :user_ad_info, only: [:show]
+
   def index
     @categories = Category.all
   end
 
   def show
-    @ad = Ad.find(params[:id])
     @category = Category.find(params[:id])
-
     @ads = @category.ads.all #can add pagination and recent
   end
+
 end
