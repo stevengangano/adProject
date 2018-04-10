b3674a1fedb5e3dedf57058ec13969dde731383c
stevengangano <stevengangano@yahoo.com>
2018-04-10 12:01:36 -0700

    added 'All' filter

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,19 @@
 class CategoriesController < ApplicationController
-  # before_action :user_ad_info, only: [:show]
+  before_action :set_sidebar_categories, only: [:index, :show]
 
   def index
     @categories = Category.all
+    @ads = Ad.published
   end
 
   def show
     @category = Category.find(params[:id])
     @ads = @category.ads.all #can add pagination and recent
+  end
+
+  private
+  def set_sidebar_categories
+    @side_bar_categories = Category.with_ads
   end
 
 end
